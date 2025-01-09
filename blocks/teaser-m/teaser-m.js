48c3609de710fe8b22673cbe33473ee49be1ddc8
Timm Reichert <t.reichert90@outloook.de>
2025-01-09 11:12:52 +0100

    add teaser

--- blocks/teaser-m/teaser-m.js
+++ blocks/teaser-m/teaser-m.js
@@ -1,15 +1,18 @@
-
 export default function decorate(block) {
-    const divs = block.children;
+    const divs = Array.from(block.children); // HTMLCollection in ein Array umwandeln
 
     const wrapperDiv = document.createElement('div');
     wrapperDiv.classList.add('teaser-m-wrapper');
 
-    const header = divs[0];
+    const header = divs[0]; // Erstes Element
 
+    // Füge alle divs (außer das erste) in den Wrapper ein
     for (let i = 1; i < divs.length; i++) {
         wrapperDiv.appendChild(divs[i]);
     }
-    divs.appendChild(header);
-    divs.appendChild(wrapperDiv);
+
+    // Block bereinigen und die beiden Elemente (header und wrapper) hinzufügen
+    block.innerHTML = ''; // Inhalt des Blocks leeren
+    block.appendChild(header); // Header hinzufügen
+    block.appendChild(wrapperDiv); // Wrapper hinzufügen
 }
