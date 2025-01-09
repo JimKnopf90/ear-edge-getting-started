4c75c389d685e4d741a660481aa2961be4b35a8a
Timm Reichert <t.reichert90@outloook.de>
2025-01-09 11:12:15 +0100

    add teaser

--- blocks/teaser-m/teaser-m.js
+++ blocks/teaser-m/teaser-m.js
@@ -1,14 +1,15 @@
+
 export default function decorate(block) {
-    const divs = Array.from(block.children);
+    const divs = block.children;
 
-    if (divs.length > 1) {
-        const wrapperDiv = document.createElement('div');
-        wrapperDiv.classList.add('teaser-m-wrapper');
+    const wrapperDiv = document.createElement('div');
+    wrapperDiv.classList.add('teaser-m-wrapper');
 
-        for (let i = 1; i < divs.length; i++) {
-            wrapperDiv.appendChild(divs[i]);
-        }
-        
-        divs[0].parentNode.insertBefore(wrapperDiv, divs[1]);
+    const header = divs[0];
+
+    for (let i = 1; i < divs.length; i++) {
+        wrapperDiv.appendChild(divs[i]);
     }
+    divs.appendChild(header);
+    divs.appendChild(wrapperDiv);
 }
