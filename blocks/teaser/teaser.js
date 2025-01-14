1b34b9fcc2fecf93f2ce4383250474d346eaf892
Timm Reichert <t.reichert90@outloook.de>
2025-01-14 18:44:23 +0100

    changes

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,21 +1,21 @@
 export default function decorate(block) {
     const divs = Array.from(block.children);
     console.log(divs);
-    const wrapperDiv = document.createElement('div');
-    wrapperDiv.classList.add('teaser-wrapper');
+    // const wrapperDiv = document.createElement('div');
+    // wrapperDiv.classList.add('teaser-wrapper');
 
-    const header = divs[0];
+    // const header = divs[0];
 
-    const h2Header = document.createElement('h2');
-    h2Header.innerHTML = header.innerHTML;
+    // const h2Header = document.createElement('h2');
+    // h2Header.innerHTML = header.innerHTML;
 
-    for (let i = 1; i < divs.length; i++) {
-        divs[i].classList.add('teaser');
-        divs[i].children[1].classList.add('teaser-title');
-        wrapperDiv.appendChild(divs[i]);
-    }
+    // for (let i = 1; i < divs.length; i++) {
+    //     divs[i].classList.add('teaser');
+    //     divs[i].children[1].classList.add('teaser-title');
+    //     wrapperDiv.appendChild(divs[i]);
+    // }
     
-    block.innerHTML = '';
-    block.appendChild(h2Header);
-    block.appendChild(wrapperDiv);
+    // block.innerHTML = '';
+    // block.appendChild(h2Header);
+    // block.appendChild(wrapperDiv);
 }
