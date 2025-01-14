19d04817e2197ec345541124b181e182b7c30c63
Timm Reichert <t.reichert90@outloook.de>
2025-01-14 18:55:29 +0100

    changes

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,21 +1,4 @@
 export default function decorate(block) {
     const divs = Array.from(block.children);
-    console.log(divs);
-    // const wrapperDiv = document.createElement('div');
-    // wrapperDiv.classList.add('teaser-wrapper');
-
-    // const header = divs[0];
-
-    // const h2Header = document.createElement('h2');
-    // h2Header.innerHTML = header.innerHTML;
-
-    // for (let i = 1; i < divs.length; i++) {
-    //     divs[i].classList.add('teaser');
-    //     divs[i].children[1].classList.add('teaser-title');
-    //     wrapperDiv.appendChild(divs[i]);
-    // }
-    
-    // block.innerHTML = '';
-    // block.appendChild(h2Header);
-    // block.appendChild(wrapperDiv);
+    console.log("Teaser Block:", divs);
 }
