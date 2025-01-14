76fb556a6c2943680964d57fa6cd674aa89aac7b
Timm Reichert <t.reichert90@outloook.de>
2025-01-14 18:41:16 +0100

    changes

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
     const divs = Array.from(block.children);
-
+    console.log(divs);
     const wrapperDiv = document.createElement('div');
     wrapperDiv.classList.add('teaser-wrapper');
 
